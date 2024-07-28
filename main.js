b782b554108200b23499c76e4d8bb85a5b4fbbc6
PacRepos <156009418+PacRepos@users.noreply.github.com>
2024-07-27 20:05:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+/*
+let div = document.querySelector('div');
+for(var i = 0; i < 10; i++){
+	let button = document.createElement('button');
+      button.innerText = i;
+  div.append(button);
+}
+*/
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -31,8 +39,8 @@ function main() {
   } else {
     clicks = Number(clicks);
   }
-  document.getElementById("clickcount").innerHTML = "You have clicked " + clicks + " times!";
-  document.getElementById("b").onclick = function click() {
+  document.getElementById("clickcount").innerHTML = "You have " + clicks + " clicks!";
+  document.getElementById("clicker").onclick = function click() {
     clicks += 1;
     document.getElementById("clickcount").innerHTML = "You have clicked " + clicks + " times!";
     setCookie("clickamt", clicks, 365);
