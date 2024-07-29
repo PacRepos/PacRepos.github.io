4011d024753b9ed4ff2b6bf984accdbf09459ead
PacRepos <156009418+PacRepos@users.noreply.github.com>
2024-07-28 19:22:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,19 +6,22 @@ for(var i = 0; i < 10; i++){
   div.append(button);
 }
 */
+
+var manualClickMult = 1;
+
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
+  var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
+  var expires = "expires="+d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') { // remove leading spaces
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
@@ -32,18 +35,38 @@ function eraseCookie(cname) {
   setCookie(cname, "", -1);
 }
 
+function addShopButtons(clicks) {
+	var div = document.getElementById("shop");
+	if (clicks >= 50) {
+		var btn = document.createElement("button");
+		div.append(btn);
+		btn.setAttribute("id", upg1);
+		document.getElementById(upg1).innerHTML = "Gain a x2 Multiplier to clicking manually for 50 clicks!";
+		document.getElementById(upg1).onclick = function() {
+			if (clicks >= 50) {
+				clicks -= 50;
+				manualClickMult *= 2;
+				btn.remove();
+			}
+		}
+	}
+}
+
 function main() {
-  let clicks = getCookie("clickamt");
+  var clicks = getCookie("clickamt");
   if (clicks == "" || clicks == null) {
     clicks = 0;
   } else {
     clicks = Number(clicks);
   }
+
+	setInterval(function() {
+		document.getElementById("clickcount").innerHTML = "You have " + clicks + " clicks!";
+		document.getElementById("clicker").innerHTML = "Get +" + manualClickMult + " Clicks!";
+		setCookie("clickamt", clicks, 365);
+	}, 10);
 	
-  document.getElementById("clickcount").innerHTML = "You have " + clicks + " clicks!";
   document.getElementById("clicker").onclick = function click() {
     clicks += 1;
-    document.getElementById("clickcount").innerHTML = "You have " + clicks + " clicks!";
-    setCookie("clickamt", clicks, 365);
   }
 }
