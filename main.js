244b4bffd6fe513dc74ede93ab8eee8202a6de45
PacRepos <156009418+PacRepos@users.noreply.github.com>
2024-09-23 11:22:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ var clicks = 0;
 var manualClickMult = 1;
 var wasCreated = false;
 
+var cBought = [false, false];
+var clickers = [0, 0];
+
+var cps = 0;
+
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -33,14 +38,27 @@ function addShopButtons() {
     if (clicks >= 50 && wasCreated == false) {
         var btn = document.createElement("button");
         div.append(btn);
-        btn.setAttribute("id", "upg1");
-        document.getElementById("upg1").innerHTML = "Gain a x2 Multiplier to clicking manually for 50 clicks!";
+        btn.setAttribute("id", "mupg1");
+        document.getElementById("mupg1").innerHTML = "Gain a x2 Multiplier to clicking manually for 50 clicks!";
         wasCreated = true;
-        document.getElementById("upg1").onclick = function() {
+        document.getElementById("mupg1").onclick = function() {
             if (clicks >= 50) {
                 clicks = clicks - 50;
                 manualClickMult *= 2;
                 this.remove();
+            }
+        }
+    }
+
+    if (clicks >= 50) {
+        var c1btn = document.createElement("button");
+        div.append(c1btn);
+        btn.setAttribute("id", "cupg1");
+        document.getElementById("cupg1").innerHTML = "Purchase an automatic tier 1 clicker that gives 1 click per second";
+        document.getElementById("cupg1").onclick = function() {
+            if (clicks >= 50) {
+                clicks = clicks - 50;
+                clickers[0]++;
             }
         }
     }
@@ -50,6 +68,7 @@ function main() {
     clicks = getCookie("clickamt");
     manualClickMult = getCookie("manualClickMult");
     wasCreated = getCookie("created");
+    cBought = getCookie("bought");
     if (manualClickMult == null || manualClickMult == "") {
         manualClickMult = 1;
     }
@@ -62,10 +81,27 @@ function main() {
     setInterval(function() {
         document.getElementById("clickcount").innerHTML = "You have " + clicks + " clicks!";
         document.getElementById("clicker").innerHTML = "Get +" + manualClickMult + " Clicks!";
+
+        for (var i = 1; i <= clickers.length; i++) {
+            cps += i * clickers[i];
+        }
+        
+        if (cBought[0]) {
+            document.getElementById("active").innerHTML = "You have " + clickers[0] + " tier 1 clickers giving " + clickers[0] + " clicks per second.");
+        } else if (cBought[1]) {
+             document.getElementById("active").innerHTML = "You have " + clickers[0] + " tier 1 clickers giving " + clickers[0] + " clicks per second.
+                 <br>
+                 You have " + clickers[1] + " tier 2 clickers giving " + (2 * clickers[1] + " clicks per second.
+                 <br>
+                 Your total clicks per second is " + cps + ".");
+        } else {
+            document.getElementById("active").innerHTML = "";
+        }
         
         setCookie("clickamt", clicks, 365);
         setCookie("manualClickMult", manualClickMult, 365);
         setCookie("created", wasCreated, 365);
+        setCookie("bought", cBought, 365);
         
         addShopButtons();
         
@@ -87,5 +123,6 @@ function main() {
         clicks = 0;
         manualClickMult = 1;
         wasCreated = false;
+        cBought = [false, false];
     }
 }
